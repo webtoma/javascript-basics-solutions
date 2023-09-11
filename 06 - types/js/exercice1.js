851ac4b053ceb48ac02a0d1d78fee117f9e53355
webtoma <thomas.delalbre@gmail.com>
2023-09-11 16:28:58 +0200

    updating submarines game

--- 06 - types/js/exercice1.js
+++ 06 - types/js/exercice1.js
@@ -1,7 +1,7 @@
 // Your code goes here
 let nombreEnChaine = "234";
-let myBoolean = true;
 let nombre = parseInt(nombreEnChaine);
+let myBoolean = true;
 console.log(typeof nombreEnChaine);
 console.log(typeof nombre);
 console.log(typeof myBoolean);
