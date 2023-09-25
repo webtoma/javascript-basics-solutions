68b58fd6f8cf67aa469e8bdb362e41a91f624f86
webtoma <thomas.delalbre@gmail.com>
2023-09-25 15:34:15 +0200

    updating tables and matrix calcs

--- 12 - advanced-arrays/js/exercice1.js
+++ 12 - advanced-arrays/js/exercice1.js
@@ -1,17 +1,17 @@
 // Your code goes here
-
+// display memory usage by a JS loop
 let tableA = [1, 2, 3, 4];
 let tableB = [100, 100, 100, 100];
 let tableC = [];
-for(let i = 0; i < tableA.length; i++){
-   let addtion = tableA[i] + tableB[i];
-   tableC[i] = addtion;  
+let lengthArray = tableA.length;
+for(let i = 0; i < lengthArray; i++){
+   tableC[i] = tableA[i] + tableB[i];  
 }
 console.log(tableC);
 
 let tableE = [];
 let tableD = [[0, 1, 2, 3, 4], [100, 100, 100, 100, 100]];
-for (let i = 0; i<tableD[0].length; i++){
+for (let i = 0; i < tableD[0].length; i++){
     let addition = tableD[0][i] + tableD[1][i];
     tableE[i] = addition;
 }
